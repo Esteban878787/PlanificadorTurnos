232f1caf8cb726c41c38219b5fa1521cac542ca4
Esteban878787 <esteban.vesov@drylocktechnologies.com>
2026-07-28 20:26:40 +0200

    Delete js directory

--- js/supabase.js
+++
@@ -1,8 +0,0 @@
-const SUPABASE_URL = "https://dkmvtvikkrhasohnmoxn.supabase.co";
-
-const SUPABASE_KEY = "sb_publishable_2OxvmkcSk7-oNz_pwbY-bQ_7F8snD4K";
-
-window.supabaseClient = supabase.createClient(
-    SUPABASE_URL,
-    SUPABASE_KEY
-);
